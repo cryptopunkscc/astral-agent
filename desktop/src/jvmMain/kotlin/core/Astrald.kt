9eff375235e150fd8aa7bff0366d6fd5feee72de
yang <cryptopunkyang@pm.me>
2023-02-12 22:32:37 +0100

    chore(desktop): Add astrald exit codes

--- desktop/src/jvmMain/kotlin/core/Astrald.kt
+++ desktop/src/jvmMain/kotlin/core/Astrald.kt
@@ -8,6 +8,14 @@ class Astrald(
 ) : Platform by platform,
     Resources by resources {
 
+    enum class Exit {
+        Success,       // Normal exit
+        Help,          // Help was invoked
+        NodeError,     // Node reported an error
+        Forced,        // User forced shutdown with double SIGINT
+        ConfigError,   // An invalid or non-existent config file provided
+    }
+
     fun start(): Process = astraldExecutable
         .apply { copyAstraldIfNeeded() }
         .exec()
